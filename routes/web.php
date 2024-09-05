becfc96a7cf07ea50fa995fa6857e65800bafe78
Mangsen <priyatmojoew@gmail.com>
2024-09-05 18:39:42 +0700

    Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/hello', function () { 
+     return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return 'Priyatmojo <br> 2241760110';
+});
+
+Route::get('/user/{name}', function ($name) { 
+    return 'Nama saya '.$name; 
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Post ke-'.$postId. "Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name='jhon') {
+    return 'Nama saya '.$name;
 });
